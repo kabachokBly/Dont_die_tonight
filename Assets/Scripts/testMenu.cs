6b734b72364ededee1a7b548833e14028892cbb8
Futsuu <everlastingtimur@gmail.com>
2025-12-17 20:50:39 +0300

    ура кнопка работает

---
+++ Assets/Scripts/testMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class testMenu : MonoBehaviour
+{
+    public void startGame()
+    {
+        Debug.Log("start!");
+    }
+    public void exitGame()
+    {
+        Debug.Log("exit");
+    }
+}
